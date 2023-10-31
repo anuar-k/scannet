2cf3cf9ad574e83afc46773a0699df674304d192
anuar <Anuar>
2023-10-31 11:21:53 +0600

    change build file name in pom.xml

--- src/main/java/com/netapp/Application.java
+++ src/main/java/com/netapp/Application.java
@@ -4,15 +4,22 @@ import com.netapp.controllers.ScannerController;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 public class Application {
 
     public void start() throws NullPointerException {
-        Javalin app = Javalin.create(config -> {
-            config.addStaticFiles("/public", Location.CLASSPATH);
-        }).start(80);
+        Javalin app = Javalin.create(config -> config.addStaticFiles("/public", Location.CLASSPATH)).start(80);
+        app.routes(() -> {
+            path("/", () -> {
+                get(ScannerController::get);
+                post(ScannerController::scan);
+            });
 
-        app.get("/", ScannerController::get);
-        app.post("/scan", ScannerController::scan);
-
+            path("/show", () -> {
+                get(ScannerController::get);
+                post(ScannerController::scan);
+            });
+        });
     }
 }
